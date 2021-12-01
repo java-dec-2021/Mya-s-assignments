4b3d128ddfc371b5e441d46096d4912ba177cc65
M_p <skyusmpp@gmail.com>
2021-11-30 22:50:55 -0800

    HashMap

---
+++ 01-java-fundamentals/07-map-of-the-hashmatique/Map.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+// import java.util.Set;
+public class Map{
+    public static void main(String[] args){
+        HashMap<String, String> song= new HashMap<String, String>();
+        song.put("Ghost", "the ghost of you ...... ghost of you");
+        song.put("Champion", "waka waka aaa");
+        song.put("friends", "we are friends");
+        song.put("woke up late", "woke up late.....");
+
+        // String name = song.get("waka waka aaa");
+        // System.out.println("Lyrics : " + name);
+
+        
+        for(HashMap.Entry<String, String> track:song.entrySet()){
+            System.out.println(track.getKey() + " : " + track.getValue());
+        
+        }
+        
+    }
+
+}
