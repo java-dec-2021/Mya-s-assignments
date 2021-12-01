ad56a069dc02157759446cd6a52874228376d97e
M_p <skyusmpp@gmail.com>
2021-12-01 15:27:25 -0800

    Basics

---
+++ 01-java-fundamentals/05-basics/BasicJava.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+public class BasicJava{
+
+    // print 1-255
+    // public void print255(){
+    //     for (int i = 1; i <= 255; i++){
+    //         System.out.println(i);
+    //     }
+    // }
+
+    // // print odd numbers between 1-255
+    // public void oddNumbers(){
+    //     for (int i = 1; i< 256; i+=2){
+    //         System.out.println(i);
+    //     }
+    
+    // }   
+    
+    // // Sigma of 255 AKA sum to 255
+    // public void sumNumbers(){
+    //     int sum=0;
+    //     for (int i = 1; i<256; i++){
+    //         sum = sum+i;
+    //         System.out.println(sum);
+    //     }
+    // }
+
+    // iterating through an array
+    // public void iterateArray(){
+    //     int[] myArray ={1,3,5,7,9,13};
+    //     for (int i = 0; i<myArray.length; i++){
+    //         System.out.println(myArray[i]);
+    // }    
+// }
+    
+    // Find max
+    // public void findMax(){
+    //     int[] Array={-3,-5,-7, 0};
+    //     int max = Array[0];
+    //     for (int i = 0; i<Array.length; i++){
+    //         if(Array[i] > max){
+    //             max = Array[i];
+    //         }
+    //     }
+    //     System.out.println(max);
+    // }
+
+    // Array with odd numbers
+    // public void arrayY(){
+    //     ArrayList<Object> y = new ArrayList<Object>();
+    //     for (int i= 1; i<=255; i+=2){
+    //         y.add(i);
+    //     }
+    //     System.out.println(y);
+    // }
+
+    // Ninja Bonus
+    // Get Average
+    // public void getAverage(){
+    //     int avg = 0;
+    //     int sum = 0;
+    //     int[] arr={2, 10, 3, 8, 12, 100};
+    //     for (int i = 0; i<arr.length; i++){
+    //         sum = sum + arr[i];
+    //         avg=sum/arr.length;
+            
+    //     }      
+    //     System.out.println(avg);
+    // }
+
+    // Greater Than Y
+    public void greaterThanY(){
+        int[] array={1,3,5,9,};
+        int y = 3;
+        for(int i = 0; i<array.length; i++){
+            if(y<array[i]){
+                System.out.println(array[i]);
+            }
+        }
+        }
+
+    // square the values
+    // public void square(){
+    //     int [] x = {1,5,10,-2,18};
+    //     ArrayList<Object> y = new ArrayList<Object>();
+    //     for (int i = 0; i< x.length; i++){
+    //         x[i]= x[i] * x[i];
+    //         y.add(x[i]);
+            
+    //     }
+    //     System.out.println(y);
+        
+    // }
+
+    // Eliminate Negative Numbers
+    // public void eliNegNum(){
+    //     ArrayList<Object> z= new ArrayList<Object>();
+    //     int[]x ={1,5,-4,10,-2};
+    //     // int value= -2;
+    //     for (int i = 0; i<x.length; i++){
+    //         if(x[i] < 0)
+    //         x[i]=0;
+    //         z.add(x[i]);
+    //     }
+    //     System.out.println(z);
+    // }
+
+    // Sensei Bonuses
+    // Max, Min, and Average
+    // public void maxMinAve(){
+    //     ArrayList<Object> z= new ArrayList<Object>();
+    //     int[]x= {1,5,10,-2};
+    //     for (int i = 0; i<3; i++){
+            
+    //     }
+    // } 
+
+    
+}
---
+++ 01-java-fundamentals/05-basics/Test.java
@@ -0,0 +1,28 @@
+// import java.util.ArrayList;
+
+public class Test{
+    public static void main (String[] args){
+       
+        BasicJava getNumbers = new BasicJava();
+        // getNumbers.print255();
+        // getNumbers.oddNumbers();
+        // getNumbers.sumNumbers();
+        // getNumbers.iterateArray();
+        // getNumbers.findMax();
+        // getNumbers.arrayY();
+        // getNumbers.getAverage();
+        // getNumbers.greaterThanY();
+        // getNumbers.square();
+        // getNumbers.eliNegNum();
+    }
+
+    
+// iterate through an array   
+//     public static void iterateArray(){
+//     int[] myArray={1,3,5,7,9,13};
+//     for (int i = 0; i<myArray.length; i++){
+//         System.out.println(myArray[i]);
+// }    
+}
+
+    
