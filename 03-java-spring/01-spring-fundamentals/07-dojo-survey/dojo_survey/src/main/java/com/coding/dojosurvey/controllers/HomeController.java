7aac536bb05136cbaf3133c49c697bcab9e8f4b6
M_p <skyusmpp@gmail.com>
2021-12-09 17:19:45 -0800

    dojo survey

--- 03-java-spring/01-spring-fundamentals/07-dojo-survey/dojo_survey/src/main/java/com/coding/dojosurvey/controllers/HomeController.java
+++ 03-java-spring/01-spring-fundamentals/07-dojo-survey/dojo_survey/src/main/java/com/coding/dojosurvey/controllers/HomeController.java
@@ -2,18 +2,25 @@ package com.coding.dojosurvey.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+//	@RequestMapping("/")
+	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("your_name", "name" );
 		return "index.jsp";
 	}
-	@RequestMapping("/result")
+//	@RequestMapping("/result")
+	
+	@RequestMapping(path="/result", method=RequestMethod.POST)
+	@PostMapping(path="/result")
 	public String result(@RequestParam("your_name") String name, @RequestParam("dojo_location") String dojo_location,
 						@RequestParam("fav_language") String fav_language,
 						@RequestParam("comment") String comment, Model model) {
