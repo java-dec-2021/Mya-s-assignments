1b343fc8dd74df306eb149e90ba8b2acb8e77f8d
M_p <skyusmpp@gmail.com>
2021-12-14 17:56:01 -0800

    the code

--- 03-java-spring/01-spring-fundamentals/06-the-code/thecode/src/main/java/com/coding/thecode/controllers/HomeController.java
+++ 03-java-spring/01-spring-fundamentals/06-the-code/thecode/src/main/java/com/coding/thecode/controllers/HomeController.java
@@ -1,23 +1,37 @@
 package com.coding.thecode.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
-
 	@RequestMapping("/")
-	public String index() {
+	public String index(@ModelAttribute("errors") String errors, Model model) {
+		System.out.println(errors);
+		model.addAttribute("error", errors);
+		
 		return "index.jsp";
 	}
+	@RequestMapping(path="/submit",  method=RequestMethod.POST)
+	public String code (@RequestParam(value="code") String code,Model model, RedirectAttributes redirect) {
+			if (code.equals("bushido")) {
+				model.addAttribute("codes", code);
+				return "redirect:/code";
+			}
+			else {
+				redirect.addFlashAttribute("errors", "You must train harder!");
+//				System.out.println("code");
+			return "redirect:/";
+			}
+	}
+	
 	@RequestMapping("/code")
-	public String code() {
+	public String codedisplay() {
 		return "code.jsp";
 	}
-	@RequestMapping("/errors")
-    public String errors(RedirectAttributes redirectAttributes){
-        redirectAttributes.addFlashAttribute("errors", "You must train harder!");
-        return "redirect:/";
-    }
 }
